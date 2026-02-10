6b6fc06e6ed07377ad2189e23a004145f66f5bd2
Sai-Janjirala <saijanjirala5411@gmail.com>
2026-02-10 11:21:03 +0530

    useState completed

---
+++ ytt_useState/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import Counter from './components/Counter'
+import TodoList from './components/TodoList'
+import Profile from './components/Profile'
+import ShopingList from './components/ShopingList'
+
+const App = () => {
+  
+
+  return (
+    <div>  
+        <Counter />
+        <TodoList />
+        <Profile />
+        <ShopingList />
+    </div>
+  )
+}
+
+export default App
